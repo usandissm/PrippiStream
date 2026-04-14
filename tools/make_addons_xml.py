d2dc0417225b1174ea577f2d7b98c5fdbc138f74
usandissm <usandissm@users.noreply.github.com>
2026-04-14 10:26:25 +0200

    Fix MD5 checksum: normalizza line endings LF, gitattributes per addons.xml

--- tools/make_addons_xml.py
+++ tools/make_addons_xml.py
@@ -1,7 +1,12 @@
 import hashlib
 import re
+import os
 
 BOM = b'\xef\xbb\xbf'
+
+# Vai nella cartella root del progetto (uno sopra tools/)
+root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+os.chdir(root)
 
 with open('addon.xml', 'rb') as f:
     addon_data = f.read()
@@ -13,6 +18,10 @@ with open('docs/repository.prippistream/addon.xml', 'rb') as f:
 if repo_data.startswith(BOM):
     repo_data = repo_data[3:]
 
+# Normalizza line endings a LF
+addon_data = addon_data.replace(b'\r\n', b'\n').replace(b'\r', b'\n')
+repo_data  = repo_data.replace(b'\r\n', b'\n').replace(b'\r', b'\n')
+
 addon_content = re.sub(rb'<\?xml[^?]*\?>\s*', b'', addon_data).strip()
 repo_content  = re.sub(rb'<\?xml[^?]*\?>\s*', b'', repo_data).strip()
 
@@ -23,7 +32,7 @@ with open('docs/addons.xml', 'wb') as f:
     f.write(addons_xml)
 
 md5 = hashlib.md5(addons_xml).hexdigest()
-with open('docs/addons.xml.md5', 'w') as f:
+with open('docs/addons.xml.md5', 'w', newline='') as f:
     f.write(md5)
 
 print('addons.xml rigenerato, md5:', md5)
